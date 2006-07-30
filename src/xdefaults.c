7c9120d2f83089c5d8338d98479b0cbc5e5479a3
jimmyzhou <jimmyzhou@b659a89f-7713-0410-90e7-b53dc03aaadd>
2006-07-30 22:32:08 +0000

    add fast memory in help message
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@121 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/xdefaults.c
+++ src/xdefaults.c
@@ -646,6 +646,9 @@ static const char optionsstring[] = "Options: "
 #ifdef OUR_STRINGS
     "strings,"
 #endif
+#ifdef OUR_MALLOC
+    "fast memory,"
+#endif
 #if !defined(NO_FRILLS)
     "frills,"
 #endif
