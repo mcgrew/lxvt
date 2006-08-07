d7856a4a8d4ad0a30738357397e7ff777ed588a3
jimmyzhou <jimmyzhou@b659a89f-7713-0410-90e7-b53dc03aaadd>
2006-08-07 04:43:11 +0000

    tabstop
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@136 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/macros.c
+++ src/macros.c
@@ -583,11 +583,11 @@ rxvt_cleanup_macros( rxvt_t *r )
 	    r->macros[i].keysym	    = 0;
 	    r->macros[i].modFlags   = 0;
 
-		if (NOT_NULL(r->macros[i].action.str))
-		{
-			rxvt_free( r->macros[i].action.str );
-			SET_NULL(r->macros[i].action.str); /* Probably unnecessary */
-		}
+	    if (NOT_NULL(r->macros[i].action.str))
+	    {
+	        rxvt_free( r->macros[i].action.str );
+	        SET_NULL(r->macros[i].action.str); /* Probably unnecessary */
+	    }
 
 	    nDummyMacros++;
 	}
