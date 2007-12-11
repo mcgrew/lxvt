0f88e0cb9d1cbea9523358e0f8d36cbec59da93d
hysseo <hysseo@b659a89f-7713-0410-90e7-b53dc03aaadd>
2007-12-11 20:28:29 +0000

    Fix codeset guess from name with modifier '@' + preferred guess method if defined HAVE_NL_LANGINFO
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@255 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/encoding.c
+++ src/encoding.c
@@ -441,14 +441,25 @@ char*
 rxvt_get_encoding_from_locale (rxvt_t* r)
 {
     char*   enc;
+	 char* end;
 
     if (IS_NULL(r->h->locale))
 	return NULL;
 
+#ifdef HAVE_NL_LANGINFO
+	if (IS_NULL (enc = nl_langinfo (CODESET)))
+		return NULL;
+#else
+	// the codeset is between '.' and '@'.
     if (IS_NULL(enc = strchr (r->h->locale, '.')))
 	return NULL;
 
+	if (!IS_NULL (end = strchr (r->h->locale, '@')))
+		end[0] = '\0';
+
     enc ++;
+#endif
+
     if ((char) 0 == *enc)
 	return NULL;
 
