592c65bc593a89bf8faf7a853b8a61e755da240b
gi1242 <gi1242@b659a89f-7713-0410-90e7-b53dc03aaadd>
2008-06-02 18:47:56 +0000

    The variable "end" is used if HAVE_NL_LANGINFO is not defined. Add back the variable "end" in this case.
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@288 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/encoding.c
+++ src/encoding.c
@@ -465,17 +465,20 @@ rxvt_get_encoding_from_locale (rxvt_t* r)
 	return NULL;
 
 #ifdef HAVE_NL_LANGINFO
-	if (IS_NULL (enc = nl_langinfo (CODESET)))
-		return NULL;
-#else
-	// the codeset is between '.' and '@'.
-    if (IS_NULL(enc = strchr (r->h->locale, '.')))
+    if (IS_NULL (enc = nl_langinfo (CODESET)))
 	return NULL;
+#else
+    {
+	char *end;
+	// the codeset is between '.' and '@'.
+	if (IS_NULL(enc = strchr (r->h->locale, '.')))
+	    return NULL;
 
 	if (!IS_NULL (end = strchr (r->h->locale, '@')))
-		end[0] = '\0';
+	    end[0] = '\0';
 
-    enc ++;
+	enc ++;
+    }
 #endif
 
     if ((char) 0 == *enc)
