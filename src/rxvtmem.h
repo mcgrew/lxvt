a629794a63969ea26816f24fec3ce919fa634be8
mschoechlin <mschoechlin@b659a89f-7713-0410-90e7-b53dc03aaadd>
2008-07-08 18:24:37 +0000

    fixed headline
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@302 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/rxvtmem.h
+++ src/rxvtmem.h
@@ -1,5 +1,5 @@
 /*--------------------------------*-H-*---------------------------------*
- * File:	grkelot.h
+ * File:	rxvtmem.h
  *----------------------------------------------------------------------*
  *
  * All portions of code are copyright by their respective author/s.
