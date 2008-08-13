f3b24e2c4dc3b45d278de2663d59e063181c1ad0
hysseo <hysseo@b659a89f-7713-0410-90e7-b53dc03aaadd>
2008-08-13 22:02:41 +0000

    Keep the original value of the filename pointer in rxvt_paste_file for case where it needed to be freed by the caller
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@325 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/screen.c
+++ src/screen.c
@@ -5063,51 +5063,53 @@ rxvt_selection_request_other(rxvt_t* r, int page, Atom target, int selnum)
 void
 rxvt_paste_file(rxvt_t* r, int page, Time tm, int x, int y, char* filename)
 {
-    rxvt_dbgmsg(( DBG_DEBUG, DBG_SCREEN,
-		"rxvt_paste_file %d (%lu, %d, %d) %s\n", page, tm, x, y,
-		filename ));
+	rxvt_dbgmsg(( DBG_DEBUG, DBG_SCREEN,
+				"rxvt_paste_file %d (%lu, %d, %d) %s\n", page, tm, x, y,
+				filename ));
 
-    if (x < 0 || x >= VT_WIDTH(r) || y < 0 || y >= VT_HEIGHT(r))
-	return;		/* outside window */
+	if (x < 0 || x >= VT_WIDTH(r) || y < 0 || y >= VT_HEIGHT(r))
+		return;		/* outside window */
 
-    char buffer[BUFSIZ];
-    char TAINTED * str;
-    FILE * fdpaste;
-
-#ifdef HAVE_WORDEXP_H
-    wordexp_t p;
-    int wordexp_result;
-
-    /* perform a shell-like expansion of the provided filename */
-    wordexp_result = wordexp(filename, &p, 0);
-    if( wordexp_result == 0 && p.we_wordc == 1 )
-	filename = *p.we_wordv;
-    else
-    {
-	rxvt_msg( DBG_ERROR, DBG_SCREEN,
-		    "Error expanding %s, or possibly ambiguous expansion\n",
-		    filename );
-	rxvt_msg( DBG_INFO, DBG_SCREEN, "wordexp_result=%i\n", wordexp_result );
-    }
-#endif
-
-    if (NOT_NULL(fdpaste = fopen( filename , "r")))
-    {
-	while (NOT_NULL(str = fgets(buffer, sizeof(buffer), fdpaste)))
-	    rxvt_paste_str( r, page, (const unsigned char*) str , STRLEN(str));
-
-	fclose(fdpaste);
-    }
-    else
-    {
-	rxvt_dbgmsg ((DBG_VERBOSE, DBG_SCREEN,
-		    "rxvt_paste_file : unable to open file '%s'\n", filename));
-    }
+	char buffer[BUFSIZ];
+	char TAINTED * str;
+	FILE * fdpaste;
 
 #ifdef HAVE_WORDEXP_H
-    wordfree(&p);
+	wordexp_t p;
+	int wordexp_result;
+	char* filename_original_ptr = filename;
+
+	/* perform a shell-like expansion of the provided filename */
+	wordexp_result = wordexp(filename, &p, 0);
+	if( wordexp_result == 0 && p.we_wordc == 1 )
+		filename = *p.we_wordv;
+	else
+	{
+		rxvt_msg( DBG_ERROR, DBG_SCREEN,
+				"Error expanding %s, or possibly ambiguous expansion\n",
+				filename );
+		rxvt_msg( DBG_INFO, DBG_SCREEN, "wordexp_result=%i\n", wordexp_result );
+	}
 #endif
-    return;
+
+	if (NOT_NULL(fdpaste = fopen( filename , "r")))
+	{
+		while (NOT_NULL(str = fgets(buffer, sizeof(buffer), fdpaste)))
+			rxvt_paste_str( r, page, (const unsigned char*) str , STRLEN(str));
+
+		fclose(fdpaste);
+	}
+	else
+	{
+		rxvt_dbgmsg ((DBG_VERBOSE, DBG_SCREEN,
+					"rxvt_paste_file : unable to open file '%s'\n", filename));
+	}
+
+#ifdef HAVE_WORDEXP_H
+	filename = filename_original_ptr;
+	wordfree(&p);
+#endif
+	return;
 }
 
 
