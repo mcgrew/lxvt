e58f92fac5dd11d2f2a19b511e03c2657d31e7d5
gi1242 <gi1242@b659a89f-7713-0410-90e7-b53dc03aaadd>
2008-09-10 05:08:14 +0000

    Fix segfault if workingDirectory is empty. (Bug introduced when we expand workingDirectory using wordexp)
    git-svn-id: http://materm.svn.sourceforge.net/svnroot/materm/mrxvt05b@338 b659a89f-7713-0410-90e7-b53dc03aaadd

--- src/tabbar.c
+++ src/tabbar.c
@@ -1327,7 +1327,7 @@ rxvt_append_page( rxvt_t* r, int profile,
 		if( p.we_wordc > 1)
 		    rxvt_msg( DBG_ERROR, DBG_TABBAR,
 			    "Too many words when expanding %s\n", cwdOption );
-		else
+		else if( NOT_NULL( *p.we_wordv ) )
 		{
 		    filename = *p.we_wordv;
 
