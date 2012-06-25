mcgrew/lxvt

commit 7a44d106490f91bbef0f72d2b1eb08d20a943177

remove printpipe
it didn't work anyway

Conflicts:
	src/screen.c

src/command.c
@@ -189,9 +189,6 @@ void           rxvt_process_propertynotify   (rxvt_t*, XEvent*);
 void           rxvt_process_expose           (rxvt_t*, XEvent*);
 void           rxvt_process_motionnotify     (rxvt_t*, XEvent*);
 void           rxvt_process_x_event          (rxvt_t*, XEvent*);
-#ifdef PRINTPIPE
-void           rxvt_process_print_pipe       (rxvt_t*, int);
-#endif
 void           rxvt_process_nonprinting      (rxvt_t*, int, unsigned char);
 void           rxvt_process_escape_vt52      (rxvt_t*, int, unsigned char);
 void           rxvt_process_escape_seq       (rxvt_t*, int);
@@ -4636,118 +4633,6 @@ rxvt_process_x_event(rxvt_t* r, XEvent *ev)
 
 /*}}} */
 
-/*{{{ print pipe */
-/*----------------------------------------------------------------------*/
-#ifdef PRINTPIPE
-/* EXTPROTO */
-FILE*
-rxvt_popen_printer( rxvt_t *r, const char *pipeName )
-{
-    FILE*   stream = popen( pipeName ? pipeName : r->h->rs[Rs_print_pipe],
-			    "w" );
-
-
-    assert( pipeName || r->h->rs[Rs_print_pipe] );
-    if (IS_NULL(stream))
-	rxvt_msg (DBG_ERROR, DBG_COMMAND, "Can't open printer pipe %s",
-		r->h->rs[Rs_print_pipe] ? r->h->rs[Rs_print_pipe] : pipeName );
-
-    return stream;
-}
-
-
-/* EXTPROTO */
-int
-rxvt_pclose_printer(FILE *stream)
-{
-    fflush(stream);
-    /* pclose() reported not to work on SunOS 4.1.3.
-     * pclose works provided SIGCHLD handler uses waitpid
-     */
-    return pclose(stream);
-}
-
-
-/*
- * simulate attached vt100 printer
- */
-/* INTPROTO */
-void
-rxvt_process_print_pipe( rxvt_t* r, int page )
-{
-    rxvt_msg (DBG_ERROR, DBG_COMMAND,  "Print pipe not implemented in this version" );
-#if 0 /* {{{ Disabled because failures of rxvt_cmd_getc() can't be handled */
-    int		readpage = page;
-    int		done;
-    FILE*	fd;
-
-
-    if (IS_NULL(fd = rxvt_popen_printer(r, NULL)))
-	return;
-
-    /*
-    ** Send all input to the printer until either ESC[4i or ESC[?4i
-    ** is received.
-    */
-    for (done = 0; !done;)
-    {
-	unsigned char   buf[8];
-	unsigned char   ch;
-	unsigned int	i, len;
-
-	if ((ch = rxvt_cmd_getc(r, &readpage)) != C0_ESC)
-	{
-	    assert (readpage == page);
-	    assert (checksum == PVTS(r, page)->checksum);
-	    if (putc(ch, fd) == EOF)
-		break;	    /* done = 1 */
-	}
-	else
-	{
-	    len = 0;
-	    buf[len++] = ch;
-
-	    if ((buf[len++] = rxvt_cmd_getc(r, &readpage)) == '[')
-	    {
-		assert (page == readpage);
-		assert (checksum == PVTS(r, page)->checksum);
-		if ((ch = rxvt_cmd_getc(r, &readpage)) == '?')
-		{
-		    assert (page == readpage);
-		    assert (checksum == PVTS(r, page)->checksum);
-		    buf[len++] = '?';
-		    ch = rxvt_cmd_getc(r, &readpage);
-		    assert (page == readpage);
-		    assert (checksum == PVTS(r, page)->checksum);
-		}
-		if ((buf[len++] = ch) == '4')
-		{
-		    if ((buf[len++]=rxvt_cmd_getc(r, &readpage))=='i')
-		    {
-			assert (page == readpage);
-			assert (checksum == PVTS(r, page)->checksum);
-			break;	/* done = 1 */
-		    }
-		}
-	    }
-
-	    for (i = 0; i < len; i++)
-	    {
-		if (putc(buf[i], fd) == EOF)
-		{
-		    done = 1;
-		    break;
-		}
-	    }
-	}
-    }
-
-    rxvt_pclose_printer(fd);
-#endif /*}}}*/
-}
-#endif		    /* PRINTPIPE */
-/*}}} */
-
 
 /* *INDENT-OFF* */
 enum {
@@ -5268,20 +5153,6 @@ rxvt_process_csi_seq(rxvt_t* r, int page)
     switch (ch)
     {
 	/* ISO/IEC 6429:1992(E) CSI sequences (defaults in parentheses) */
-#ifdef PRINTPIPE
-	case CSI_MC:	    /* 8.3.83: (0) MEDIA COPY */
-	    switch (arg[0])
-	    {
-		case 0:		/* initiate transfer to primary aux device */
-		    rxvt_scr_printscreen( r, page, 0, 0, 0, NULL );
-		    break;
-		case 5:		/* start relay to primary aux device */
-		    rxvt_process_print_pipe( r, page );
-		    break;
-	    }
-	    break;
-#endif
-
 	case CSI_CUU:	    /* 8.3.22: (1) CURSOR UP */
 	case CSI_VPR:	    /* 8.3.161: (1) LINE POSITION FORWARD */
 	    arg[0] = -arg[0];

src/feature.h
@@ -155,11 +155,6 @@
 /* #define NO_SLOW_LINK_SUPPORT */
 
 /*
- * Printer pipe which will be used for emulation of attached vt100 printer
- */
-#define PRINTPIPE   "lpr"
-
-/*
  * Allow 80/132 mode switching on startup
  */
 /* #define ALLOW_132_MODE */

src/init.c
@@ -1242,10 +1242,6 @@ rxvt_init_resources(rxvt_t* r, int argc, const char *const *argv)
     r->h->blinkInterval *= 1000;
 #endif
 
-#ifdef PRINTPIPE
-    if (!rs[Rs_print_pipe])
-	rs[Rs_print_pipe] = PRINTPIPE;
-#endif
     if (!rs[Rs_cutchars])
 	rs[Rs_cutchars] = CUTCHARS;
 
@@ -1723,17 +1719,6 @@ rxvt_init_command(rxvt_t* r)
      */
     act.sa_handler  = SIG_IGN;
     sigaction( SIGHUP, &act, NULL);
-
-#ifdef PRINTPIPE
-    /*
-     * 2006-04-28 gi1242: If there is an error opening the printer command, then
-     * we'll get SIGPIPE. If not handled, mrxvt will exit.
-     * 
-     * There's nothing we really need to do on broken pipes, so just ignore
-     * SIGPIPE for now.
-     */
-    sigaction( SIGPIPE, &act, NULL);
-#endif
 
     act.sa_handler  = rxvt_Child_signal;
     sigaction (SIGCHLD, &act, NULL);

src/macros.c
@@ -55,7 +55,6 @@ static const char *const macroNames[] =
     "Raise",		    /* Raise the terminal window */
     "SetTitle",		    /* Set title of active tab to selection */
     "UseFifo",		    /* Enable / disable using fifo */
-    "PrintScreen",	    /* Dump screen to file / printer */
     "SaveConfig",	    /* Save config to file */
     "ToggleMacros"	    /* Toggle using keyboard macros */
 };
@@ -1349,48 +1348,6 @@ rxvt_dispatch_action( rxvt_t *r, action_t *action, XEvent *ev)
 	    }
 	    break;
 #endif/*USE_FIFO*/
-
-	case MacroFnPrintScreen:
-	{
-	    /*
-	     * Arguments: [-{s,n,p}] command
-	     *	s: Dump whole scroll back buffer.
-	     *	p: Pretty print (i.e. with escape sequences for ANSII color)
-	     *	n: No line continuation
-	     * scroll back buffer. The argument if any is the command to use for
-	     * the printer pipe.
-	     */
-
-	    char    *s		= (char*) astr;
-	    int	    pretty	= 0,
-		    scrollback	= 0,
-		    linecont	= 1;
-
-
-	    if( *s && *s == '-' )
-	    {
-		while( *(++s) && !isspace( *s ) )
-		{
-		    switch( *s )
-		    {
-			case 's': scrollback	= 1; break;
-			case 'p': pretty	= 1; break;
-			case 'n': linecont	= 0; break;
-			default:
-			    rxvt_msg (DBG_ERROR, DBG_MACROS,  "Bad option %c to macro %s",
-				    *s, macroNames[action->type] );
-			    retval = -1;
-		    }
-		}
-
-		while( isspace(*s) ) s++;
-	    }
-
-	    rxvt_scr_printscreen( r, ATAB(r), scrollback, pretty, linecont,
-		    *s ? s : NULL );
-
-	    break;
-	}
 
 	case MacroFnSaveConfig:
 	{

src/rxvt.h
@@ -1193,9 +1193,6 @@ enum {
     Rs_delete_key,
 #endif
     Rs_selectstyle,
-#ifdef PRINTPIPE
-    Rs_print_pipe,
-#endif
 #ifdef USE_XIM
     Rs_preeditType,
     Rs_inputMethod,

src/rxvtlib.h
@@ -842,7 +842,6 @@ enum
     MacroFnRaise,
     MacroFnSetTitle,
     MacroFnUseFifo,
-    MacroFnPrintScreen,
     MacroFnSaveConfig,
     MacroFnToggleMacros,
     NMACRO_FUNCS

src/screen.c
@@ -2493,161 +2493,6 @@ rxvt_scr_bell(rxvt_t *r, int page)
 
 /* ------------------------------------------------------------------------- */
 
-#ifdef PRINTPIPE
-/*
- * Generate escape sequences (not including the "\e[0") to reproduce screen
- * rendition attributes for the foreground / background color. If "fg" is true,
- * then sequences for the foreground color are generated, otherwise sequences
- * for setting the background color are generated.
- *
- * Returns a pointer to the character after the escape sequence written. If no
- * further attributes are to be added, strip the trailing ";".
- */
-/* INTPROTO */
-char *
-escSetColor( char *s, int color, int fg)
-{
-    if( color >= minCOLOR && color < minCOLOR + 8 )
-	s += sprintf( s, "%c%d;", fg ? '3' : '4', color - minCOLOR );
-#ifndef NO_BRIGHTCOLOR
-    else if( color >= minBrightCOLOR && color <= maxBrightCOLOR )
-	s += sprintf( s, "%s%d;", fg ? "9" : "10", color - minBrightCOLOR );
-#endif
-#ifdef TTY_256COLOR
-    else if( color >= min256COLOR && color <= max256COLOR )
-	s += sprintf( s, "%c8;5;%d;", fg ? '3' : '4',
-				color - min256COLOR + 16 );
-#endif
-    else
-	assert(0);
-
-    return s;
-}
-#endif
-
-/*
- * Print the screen into the printer pipe. If fullhist != 0, then the entire
- * scroll back buffer is also dumped.
- */
-/* EXTPROTO */
-void
-rxvt_scr_printscreen(rxvt_t* r, int page, int fullhist, int pretty,
-	int linecont, const char *pipeName )
-{
-#ifdef PRINTPIPE
-    int		row, col, nrows, row_offset;
-    text_t	*txt;
-    rend_t	*rnd;
-
-    FILE*	fd;
-
-
-    rxvt_dbgmsg ((DBG_DEBUG, DBG_SCREEN, "rxvt_scr_printscreen( r, %d, %d, %d, %s )\n", page, fullhist, pretty, pipeName ));
-
-    if ( ( fd = rxvt_popen_printer( r, pipeName ) ) == NULL )
-	return;
-
-    nrows	= r->TermWin.nrow;
-    row_offset	= SVLINES;
-    if (!fullhist)
-	row_offset -= VSTART;
-    else
-    {
-	nrows += PVTS(r, page)->nscrolled;
-	row_offset -= PVTS(r, page)->nscrolled;
-    }
-
-    for( row=0; row < nrows && !ferror( fd ); row++ )
-    {
-	int lineEnd;
-
-	txt = PSCR( r, page ).text[ row + row_offset ];
-	rnd = PSCR( r, page ).rend[ row + row_offset ];
-
-	if( linecont && PSCR(r, page).tlen[row+row_offset] == -1 )
-	    /* Line continues over. Don't trim trailing spaces */
-	    lineEnd = r->TermWin.ncol - 1;
-	else
-	{
-	    /* Trim trailing spaces */
-	    for(
-		 lineEnd = r->TermWin.ncol - 1;
-		 lineEnd >= 0 && isspace( txt[lineEnd] );
-		 lineEnd--
-	       )
-	    ;
-	}
-
-	if( pretty )
-	{
-	    /* Print colors as escape sequences */
-
-	    for( col=0; col <= lineEnd; )
-	    {
-		char	escsq[32];  /* Buffer to hold the escape sequence. 29
-				       bytes are enough. */
-		char	*s, *t;
-		int	start = col;
-		int	color;
-
-		rend_t	rend = rnd[start];
-
-		/* Get longest string with constant rendition attrs */
-
-		do
-		  {
-		    col++;
-		  }
-		while( col <= lineEnd && rnd[col] == rend );
-
-
-		t = s = escsq + sprintf( escsq, "\e[" );
-
-		if( rend & RS_Bold )	    s += sprintf( s, "1;" );
-		if( rend & RS_Uline )	    s += sprintf( s, "4;" );
-		if( rend & RS_Blink )	    s += sprintf( s, "5;" );
-		if( rend & RS_RVid )	    s += sprintf( s, "7;" );
-
-		color = GET_BASEFG( rend );
-		if( color != Color_fg )
-		    s = escSetColor( s, color, 1 );
-
-		color = GET_BASEBG( rend );
-		if( color != Color_bg )
-		    s = escSetColor( s, color, 0 );
-
-		if( s != t )
-		{
-		    /*
-		     * Some esc seq has been set. Null terminate and Replace
-		     * trailing ';' with 'm'
-		     */
-		    *(s--)  = '\0';
-		    *s	    = 'm';
-
-		    fprintf( fd, "%s%.*s\e[0m", escsq, col-start, txt+start );
-		}
-
-		else
-		    fprintf( fd, "%.*s", col - start, txt + start );
-
-	    }
-	} /* if( pretty ) */
-
-	else
-	    /* Vanilla text */
-	    fprintf( fd, "%.*s", (lineEnd + 1), txt );
-
-	if( !linecont || PSCR(r, page).tlen[row+row_offset] != -1 )
-	    fputc( '\n', fd );
-
-    } /* for( row ... ) */
-
-    rxvt_pclose_printer(fd);
-#endif
-}
-
-
 #ifdef TEXT_SHADOW
 /*
  * If refreshRegion is not None, then we should intersect our clipping with it.

src/xdefaults.c
@@ -527,10 +527,6 @@ static const struct {
     RSTRG(Rs_selectstyle, "selectStyle",
 	"select style mode = old|oldword", 0),
 
-#ifdef PRINTPIPE
-    RSTRG(Rs_print_pipe, "printPipe", "string", 0),
-#endif
-
     BOOL( "noSysConfig", "nsc", Opt2_noSysConfig,
 	    "reading /etc/mrxvt/mrxvtrc." ),
     BOOL( "disableMacros", "dm", Opt2_disableMacros,

commit 70b30b2dd32075b476079abeb7acbe7fcc9f975a

Fixing another regression relating to tab height.
Another regression from 7a44d106490f91bbef0f72d2b1eb08d20a943177

.gitignore
@@ -14,3 +14,4 @@ Makefile.in
 /aclocal.m4
 /install-sh
 /ChangeLog
+/src/lxvt

src/command.c
@@ -3704,9 +3704,6 @@ rxvt_calc_colrow (rxvt_t* r, unsigned int width, unsigned int height)
     r->szHint.width = width;
     r->szHint.height = height;
 
-    r->h->window_vt_x = (width - Width2Pixel(ncol))/2 - r->TermWin.int_bwidth;
-    r->h->window_vt_y = (height - Height2Pixel(nrow) + 1)/2 - r->TermWin.int_bwidth;
-
     return ((r->h->prev_ncol != r->TermWin.ncol) ||
 	    (r->h->prev_nrow != r->TermWin.nrow));
 }